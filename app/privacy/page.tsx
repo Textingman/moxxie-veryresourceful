bf88702a474c12d72fc6632abd23644a94da00d8
Textingman <matt.morf@textvolt.com>
2026-07-01 18:41:52 -0400

    Initial site setup: Very Resourceful

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -48,7 +48,7 @@ export default function PrivacyPage() {
           <p className="mb-6" style={{ color: '#6B7280' }}>
             If you have questions about this Privacy Policy, please contact us at:<br />
             Very Resourceful<br />
-            8 The Green, Ste A, Dover, DE 19901<br />
+            4 Peddlers Row #1068, Newark, DE 19702<br />
             Email: <a href="mailto:support@veryresourceful.com" style={{ color: '#1D4ED8' }}>support@veryresourceful.com</a>
           </p>
         </div>

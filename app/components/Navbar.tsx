60779d872e8e7378b5ce9b7e34accd8aae779c17
Textingman <matt.morf@textvolt.com>
2026-07-01 13:47:54 -0400

    Initial site setup: Very Resourceful

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -36,7 +36,7 @@ export default function Navbar() {
                 Terms
               </Link>
               <Link href="/signup" className="text-white px-4 py-2 rounded-md text-sm font-medium" style={{ backgroundColor: '#1D4ED8' }}>
-                Explore Resources
+                Get Family Support
               </Link>
             </div>
           </div>
